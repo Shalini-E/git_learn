d88bbad2ab884d63dd46b04e2beaab77898de7d4
Shalini-E <shalinieswaran19042004@gmail.com>
2026-06-14 17:17:50 +0530

    new

--- gcd.java
+++ gcd.java
@@ -17,3 +17,22 @@ class Main {
     System.out.println("GCD of " + n1 +" and " + n2 + " is " + gcd);
   }
 }
+
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
+}
